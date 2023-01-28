bd81a33c9bd858913809f42818dece08d538bc0b
lifeilin01 <lifeilin01@58.com>
2023-01-28 22:25:04 +0800

    feat:适配新版抖音接口

--- douyin/douyin.go
+++ douyin/douyin.go
@@ -1,21 +1,34 @@
 package douyin
 
 import (
+	"encoding/json"
 	"errors"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"regexp"
-	"strings"
+	"time"
 
 	"github.com/beego/beego/v2/core/logs"
-	"github.com/tidwall/gjson"
 )
 
 var (
 	patternStr       = `http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+`
 	DefaultUserAgent = `Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1`
-	relRrlStr        = `https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=`
+	//relRrlStr        = `https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?reflow_source=reflow_page&item_ids=`
+	relRrlStr = `https://www.iesdouyin.com/aweme/v1/web/aweme/detail/?aid=1128&version_name=23.5.0&device_platform=android&os_version=2333&aweme_id=`
+	//apiStr    = `https://aweme.snssdk.com/aweme/v1/play/?radio=1080p&line=0&video_id=`
+	src = rand.NewSource(time.Now().UnixNano())
+)
+
+const (
+	// 6 bits to represent a letter index
+	letterIdBits = 6
+	// All 1-bits as many as letterIdBits
+	letterIdMask = 1<<letterIdBits - 1
+	letterIdMax  = 63 / letterIdBits
+	letters      = "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789="
 )
 
 type DouYin struct {
@@ -65,6 +78,22 @@ func (d *DouYin) GetRedirectUrl(urlStr string) (string, error) {
 	if result == "" {
 		return "", errors.New("解析参数失败 ->" + string(body))
 	}
+	//videoUrl := apiStr + result
+	//
+	//req, err = http.NewRequest(http.MethodGet, videoUrl, nil)
+	//req.Header = header
+	//resp, err = http.DefaultTransport.RoundTrip(req)
+	//if err != nil {
+	//	return "", err
+	//}
+	//defer resp.Body.Close()
+	//
+	//body, err = io.ReadAll(resp.Body)
+	//if err != nil {
+	//	return "", err
+	//}
+	//log.Println(string(body))
+
 	return relRrlStr + result, nil
 }
 
@@ -74,6 +103,9 @@ func (d *DouYin) GetVideoInfo(urlStr string) (string, error) {
 		return "", err
 	}
 	req.Header.Add("User-Agent", DefaultUserAgent)
+	cookie := &http.Cookie{Name: "msToken", Value: d.generateRandomStr(107), HttpOnly: true}
+
+	req.AddCookie(cookie)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
@@ -89,6 +121,23 @@ func (d *DouYin) GetVideoInfo(urlStr string) (string, error) {
 	return string(body), nil
 }
 
+func (d *DouYin) generateRandomStr(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdMax letters!
+	for i, cache, remain := n-1, src.Int63(), letterIdMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdMax
+		}
+		if idx := int(cache & letterIdMask); idx < len(letters) {
+			b[i] = letters[idx]
+			i--
+		}
+		cache >>= letterIdBits
+		remain--
+	}
+	return string(b)
+}
+
 func (d *DouYin) Get(shardContent string) (Video, error) {
 	urlStr := d.pattern.FindString(shardContent)
 	if urlStr == "" {
@@ -98,15 +147,19 @@ func (d *DouYin) Get(shardContent string) (Video, error) {
 	if err != nil {
 		return Video{}, err
 	}
+	d.printf("视频链接地址 -> [url=%s]", rawUrlStr)
 	body, err := d.GetVideoInfo(rawUrlStr)
 	if err != nil {
 		return Video{}, err
 	}
 	d.printf("获取抖音视频成功 -> [resp=%s]", body)
+	var result DouYinResult
 
-	item := gjson.Get(body, "item_list.0")
+	if err := json.Unmarshal([]byte(body), &result); err != nil {
+		d.printf("解析抖音结果失败 -> [err=%s]", err)
+		return Video{}, err
+	}
 
-	res := item.Get("video.play_addr.url_list.0")
 	video := Video{
 		RawLink:      shardContent,
 		VideoRawAddr: urlStr,
@@ -114,93 +167,49 @@ func (d *DouYin) Get(shardContent string) (Video, error) {
 		Images:       []ImageItem{},
 	}
 
-	if !res.Exists() {
-		d.printf("解析抖音视频地址失败 -> [resp=%s]", body)
-		return video, errors.New("未找到视频地址 ->" + urlStr)
-	}
+	video.PlayAddr = result.AwemeDetail.Video.PlayAddr.UrlList[0]
 
-	video.PlayAddr = strings.ReplaceAll(res.Str, "playwm", "play")
-	res = item.Get("duration")
-	d.printf("视频时长 [duration=%s]", res.Raw)
+	d.printf("视频时长 [duration=%s]", result.AwemeDetail.Duration)
 	//获取播放时长，视频有播放时长，图文类无播放时长
-	if res.Exists() && res.Raw != "0" {
+	if result.AwemeDetail.Duration > 0 {
 		video.VideoType = VideoPlayType
 	} else {
 		video.VideoType = ImagePlayType
-		res = item.Get("images")
-		if res.Exists() && res.IsArray() {
-			for _, image := range res.Array() {
-				imageRes := image.Get("url_list.0")
-				if imageRes.Exists() {
-					video.Images = append(video.Images, ImageItem{
-						ImageUrl: imageRes.Str,
-						ImageId:  image.Get("uri").Str,
-					})
-				}
-			}
-		}
 	}
 	//获取播放地址
-	res = item.Get("video.play_addr.uri")
-	if res.Exists() {
-		video.PlayId = res.Str
-	}
+	video.PlayId = result.AwemeDetail.Video.PlayAddr.Uri
+
 	//获取视频唯一id
-	res = item.Get("aweme_id")
-	d.printf("唯一ID [aweme_id=%s]", res.Raw)
-	if res.Exists() {
-		video.VideoId = res.Str
-	}
+	d.printf("唯一ID [aweme_id=%s]", result.AwemeDetail.AwemeId)
+	video.VideoId = result.AwemeDetail.AwemeId
+
 	//获取封面
-	res = item.Get("video.cover.url_list.0")
-	if res.Exists() {
-		video.Cover = res.Str
-	}
+	video.Cover = result.AwemeDetail.Video.Cover.UrlList[0]
+
 	//获取原始封面
-	res = item.Get("video.origin_cover.url_list.0")
-	if res.Exists() {
-		video.OriginCover = res.Str
-	}
-	res = item.Get("video.origin_cover.url_list")
-	if res.Exists() {
-		res.ForEach(func(key, value gjson.Result) bool {
-			video.OriginCoverList = append(video.OriginCoverList, value.Str)
-			return true
-		})
-		d.printf("所有原始封面： %+v", video.OriginCoverList)
-	}
+	video.OriginCover = result.AwemeDetail.Video.OriginCover.UrlList[0]
+
+	video.OriginCoverList = result.AwemeDetail.Video.OriginCover.UrlList
+	d.printf("所有原始封面： %+v", video.OriginCoverList)
+
 	//获取音乐地址
-	res = item.Get("music.play_url.url_list.0")
-	if res.Exists() {
-		video.MusicAddr = res.Str
-	}
+	video.MusicAddr = result.AwemeDetail.Music.PlayUrl.UrlList[0]
+
 	//获取作者id
-	res = item.Get("author.uid")
-	if res.Exists() {
-		video.Author.Id = res.Str
-	}
-	res = item.Get("author.short_id")
-	if res.Exists() {
-		video.Author.ShortId = res.Str
-	}
-	res = item.Get("author.nickname")
-	if res.Exists() {
-		video.Author.Nickname = res.Str
-	}
-	res = item.Get("author.signature")
-	if res.Exists() {
-		video.Author.Signature = res.Str
-	}
+	video.Author.Id = result.AwemeDetail.Author.Uid
+
+	video.Author.ShortId = result.AwemeDetail.Author.ShortId
+
+	video.Author.Nickname = result.AwemeDetail.Author.Nickname
+
+	video.Author.Signature = result.AwemeDetail.Author.Signature
+
 	//获取视频描述
-	res = item.Get("desc")
-	if res.Exists() {
-		video.Desc = res.Str
-	}
+	video.Desc = result.AwemeDetail.Desc
+
 	//回获取作者大头像
-	res = item.Get("author.avatar_larger.url_list.0")
-	if res.Exists() {
-		video.Author.AvatarLarger = res.Str
-	}
+	video.Author.AvatarLarger = result.AwemeDetail.Author.AvatarThumb.UrlList[0]
+
 	d.printf("解析后数据 [video=%s]", video.String())
 	return video, nil
 }
--- douyin/douyin_test.go
+++ douyin/douyin_test.go
@@ -2,19 +2,26 @@ package douyin
 
 import (
 	"github.com/smartystreets/goconvey/convey"
+	"log"
 	"testing"
 )
 
 func TestDouYin_Get(t *testing.T) {
-	convey.Convey("DouYin_Get",t, func() {
+	convey.Convey("DouYin_Get", t, func() {
 		content := "5.87 WZZ:/ 再见少年拉弓满、不惧岁月不惧风！  https://v.douyin.com/85MyVfe/ 复制此链接，达开Douyin搜索，矗接观看视pin！ oxBCQt9rsUybLpUJ0BqHYk1SWZR4"
 
+		content = "{9.25 Xzg:/ 复制打开抖音，看看【第十八年冬.的作品】“我从来不信什么天道，只信我自己”# 台词 # 好... https://v.douyin.com/BeSveAc/ oxBCQt9rsUybLpUJ0BqHYk1SWZR4}"
 		dy := NewDouYin()
 
 		convey.Convey("DouYin_Get_OK", func() {
-			video,err := dy.Get(content)
-			convey.So(err,convey.ShouldBeNil)
-			convey.So(video,convey.ShouldNotBeNil)
+			video, err := dy.Get(content)
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(video, convey.ShouldNotBeNil)
 		})
 	})
+}
+
+func TestMain(m *testing.M) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	m.Run()
 }
